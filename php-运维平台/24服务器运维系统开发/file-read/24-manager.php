95248c966d5b63dd69e8fbeda95df4a25230ebe1
knight <624867243@qq.com>
2015-08-09 03:32:19 +0800

    4

--- php-运维平台/24服务器运维系统开发/file-read/24-manager.php
+++ php-运维平台/24服务器运维系统开发/file-read/24-manager.php
@@ -1,4 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<link rel="stylesheet" type="text/css" href="styles/1/basic.css"/>
+<head>
+<meta charset="utf-8" />
+<meta http-equiv="refresh" content="3">
+</head>
+<title>运维系统</title>
 <style type="text/css">
+
 <!--
 #Layer1 {
 	position:absolute;
@@ -55,33 +64,20 @@
 }
 -->
 </style>
-<head>
-<meta charset="utf-8" />
-
-<!--
-  <meta http-equiv="refresh" content="3">     每3秒刷新一次整个页面
-
-  -->
-  <meta http-equiv="refresh" content="3">
-
-</head>
-
 <div id="Layer2">
-
   <?php
 /*
-利用数组来实现读取文件最后的n行
+数组读取末尾行
 
 */
-//header('Location: http://127.0.0.1/file-read/');   //url重定向
+//header('Location: http://127.0.0.1/file-read/');  
 
-$num=10;     //表示读取最后n行
-$file="D:/phpStudy/WWW/file-read/file.txt";   //读取指定的文件
+$num=10;   
+$file="D:/phpStudy/WWW/file-read/file.txt"; 
 //echo $file;
 
 //$fh = fopen('file.txt','r');
 $fh = fopen("$file",'r');
-
 $r = array();
 while($l = fgets($fh)){
 array_push($r,$l);
@@ -94,35 +90,29 @@ file_put_contents('memory.txt',memory_get_usage());
 //var_dump($r);
 //echo $r;
 
-//访问数组成员
-
 for($i=0;$i < count($r);$i++) {
-
-echo $r[$i].'<br />';   //换行符
-
+echo $r[$i].'<br />';   
 }
-
-
-
 ?>
+
 </div>
 <div id="Layer4">
   
-  <div align="left">
+ <div align="left">
     <input name="button4" style="width:120px;height:60px;" type="button" onclick="history.go(0)" value="start 24web" />
-  </div>
+ </div>
 </div>
-
 <div id="Layer5">
   
-  <div align="left">
-    <!--
+ <div align="left">
+ <!--
 	<input name="button3" style="width:120px;height:60px;" type="button" onclick="history.go(0)" value="stop 24web" />
-	������һ��php����
+
 	
 		<input style="width:120px;height:60px;" onClick="javascript:window.location.href='test/stop-24web.php';" type="button" name="stop" value="stop 24web" />
-		-->
-		<script>	
+-->
+
+<script>	
 function load(){
 	//下面两种方法效果是一样的
 	document.getElementById("target").onclick();	
@@ -131,14 +121,12 @@ function test(){
 	alert("你要stop掉24-web的resin吗？如果是，请点击确定");
 	window.location.href='test/stop-24web.php';	
 }
-	</script>
+</script>
 	
 <div id="Layer7" onload="load()">		
 <button id="target" style="width:120px;height:60px;" onclick="test()">stop 24web</button>
 </div>
-
-
-	
-	
-  </div>
 </div>
+</div>
+
+</html>
