b26771fa49a15c2bf6fe178d08880a20a6787fd7
knight <624867243@qq.com>
2016-03-07 12:00:41 +0800

    php的curl模拟登录

---
+++ 模拟登录/login1.php
@@ -0,0 +1,32 @@
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<?php 
+
+
+$mysqli=mysqli_connect("localhost","root","root","logindb");
+$name=$_POST["name"]; 
+$passowrd=$_POST["password"]; 
+//print_r($_COOKIE);
+
+if ($name && $passowrd){ 
+$sql = "SELECT * FROM test1 WHERE name='$name' and password='$passowrd'"; 
+$res = mysqli_query($mysqli,$sql); 
+$rows=mysqli_num_rows($res); 
+if($rows == '1'){ 
+//header("location:login1.php"); 
+//exit; 
+echo "success";
+
+//echo函数单纯显示文本信息或变量值，没有返回值
+//print(包括printf,sprintf)函数除了显示之外，还会返回一个布尔值
+
+}
+ else{
+
+ //return "fdaf";
+echo "failed，please check username and password！";
+}
+mysqli_free_result($res);
+mysqli_close($mysqli);
+}
+?> 
---
+++ 模拟登录/moni.php
@@ -0,0 +1,19 @@
+<?php
+/*
+以下是模拟登录
+*/
+$url = "http://127.0.0.1/gongji2/login/login1.php";
+$post_data= array(
+    "name" => "knightd",
+    "password" => "knight",
+);
+	//$post_data = array ("s"=>'success');
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+	$output = curl_exec($ch);
+	curl_close($ch);
+	echo $output;
+?>
